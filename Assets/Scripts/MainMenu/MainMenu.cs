24b729c50ee27e12c9ad2065f1212262279a6683
AgustinIzaguirre <Agustinizag1@gmail.com>
2020-10-22 17:52:49 -0300

    Adding main menu (#7)
    * Add Main menu Scene and cameraMovement script
    * Add buttons highlighting and move script inside MainMenu folder
    * Add quit functionality to quit button on MainMenu
    * Remove extra AudioListeners from Scene to remove warnings

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HostGame()
+    {
+        Debug.Log("Host game not implemented yet");
+    }
+
+    public void JoinGame()
+    {
+        Debug.Log("Join game not implemented yet");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
